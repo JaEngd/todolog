698954c4114f0deb115519d3435a547173d9dcd6
Jacob <jacob@engd.se>
2023-01-31 22:22:35 +0100

    user model

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -26,7 +26,7 @@ router.post("/", async(req, res) => {
     })
 
     const { error } = schema.validate(req.body) 
-    
+
     if (error) return res.status(400).send(error.details[0].message)
 
     const { name, author, isComplete, date, uid } = req.body
@@ -61,13 +61,14 @@ router.put("/:id", async (req, res) => {
     
     if (error) return res.status(400).send(error.details[0].message)
 
+    try {
     const todo = await Todo.findById(req.params.id)
 
     if(!todo) return res.status(404).send("Todo not found...")
 
     const { name, author, isComplete, date, uid } = req.body
 
-    try {
+    
         const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, { 
             name, 
             author, 
@@ -84,12 +85,13 @@ router.put("/:id", async (req, res) => {
 })
 
 router.patch("/:id", async (req, res) => {
-    const todo = await Todo.findById(req.params.id)
-
-    if(!todo) return res.status(404).send("Todo not found...")
-
     try {
-    const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, {
+    
+        const todo = await Todo.findById(req.params.id)
+
+        if(!todo) return res.status(404).send("Todo not found...")
+
+        const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, {
         isComplete: !todo.isComplete
     })
         res.send(updatedTodo)
@@ -100,11 +102,11 @@ router.patch("/:id", async (req, res) => {
 })
 
 router.delete("/:id", async (req, res) => {
-    const todo = await Todo.findById(req.params.id)
-
-    if(!todo) return res.status(404).send("Todo not found...")
-
     try {
+        const todo = await Todo.findById(req.params.id)
+
+        if(!todo) return res.status(404).send("Todo not found...")
+
         const deletedTodo = await Todo.findByIdAndDelete(req.params.id)
 
         res.send(deletedTodo)
