a49f2048254876fa95266ba2c8928ae32395c77d
Jacob <jacob@engd.se>
2023-03-14 23:18:43 +0100

    Redux reducers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import { configureStore } from "@reduxjs/toolkit";
+
+import rootReducer from './store/reducers/rootReducer';
+
+const store = configureStore({
+  reducer: {
+   root: rootReducer  
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store = { store }>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
